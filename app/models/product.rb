03ee8a342f548b81952c5f8e10798915879b6d14
ArmandoKenneth <armando.kenneth@gmail.com>
2017-06-19 01:05:04 -0300

    Merged the logic to scrap different types of reviews

--- app/models/product.rb
+++ app/models/product.rb
@@ -12,50 +12,64 @@ class Product < ApplicationRecord
   end
 
   def load_reviews_and_rating
-    byebug
+    # byebug
     if self.review_valid_until.nil? || Time.now >= self.review_valid_until
       page = AmazonApi.load_from_url(self.review_url)
       review_img = page.css('.asinReviewsSummary img').first
       self.rating = review_img.nil? ? 0 : review_img.attributes['title'].value
       reviews = Array.new
       # Only works if reviews do not have the read more option
-      page.css('.reviewText').each do |content|
-        review = Review.new
-        review.review = content.text
-        previous = content
-        (0..3).each do |i|
-          previous = previous.previous_element
-          if i == 2
-            review.reviewer = previous.search('span').first.text
-          end
-        end
-        review.title = previous.search('b').first.text.strip
-        review.date = Date.parse(previous.search('nobr').first.text)
-        review.rating = previous.search('img').first.attributes['title'].value.split(" ")[0]
-        reviews << review
-      end
+      reviewers = Array.new
+      # page.css('.reviewText').each do |content|
+      #   review = Review.new
+      #   review.review = content.text
+      #   previous = content
+      #   (0..3).each do |i|
+      #     previous = previous.previous_element
+      #     if i == 2
+      #       review.reviewer = previous.search('span').first.text
+      #       reviewers << review.reviewer
+      #     end
+      #   end
+      #   review.title = previous.search('b').first.text.strip
+      #   review.date = Date.parse(previous.search('nobr').first.text)
+      #   review.rating = previous.search('img').first.attributes['title'].value.split(" ")[0]
+      #   reviews << review
+      # end
       # Reviews may have the read more option. In that case, search again, because the HTML structure
       # is different
       if reviews.length < 3
         pending_reviews = page.css('.crIFrameReviewList').search('table > tr > td > div')
-        page.css('.crIFrameReviewList').search('table > tr > td > div > div').remove
-
+        
         (0..(pending_reviews.length-1)).each do |i|
           # byebug
-          text = clean_text(page.css('.crIFrameReviewList').search('table > tr > td > div')[i].text)
-          # byebug
-          if text.length > 0
-            review = Review.new
-            review.title = i
-            review.review = text
-            reviews << review
+          css_text = pending_reviews[i].search('.reviewText').text
+          text = ''
+          if css_text.length == 0
+            text = clean_text(pending_reviews[i].text)
+          end
+          if css_text.length > 0 || text.length > 0
+            # Prevent duplicated reviews
+            if !reviewers.include? pending_reviews.search('div>div>div>a>span').first.text
+              review = Review.new
+              review.date = Date.parse(pending_reviews.search('div>nobr').first.text)
+              review.title = pending_reviews.search('div>b').first.text
+              review.reviewer = pending_reviews.search('div>div>div>a>span').first.text
+              # byebug
+              review.rating = pending_reviews.search('img').first.attributes['title'].value.split(" ")[0]
+              pending_reviews[i].search('div').remove
+              # text = pending_reviews[i].search('.reviewText').text
+              # if text.length == 0
+              #   text = clean_text(pending_reviews[i].text)
+              # end
+              review.review = css_text.length > 0 ? css_text : clean_text(pending_reviews[i].text)
+              reviews << review
+            end
           end
         end
       end
-      # byebug
       self.review_valid_until = Time.now + 24.hours
       self.reviews = reviews
-      # self.reviews
     end
   end
 
@@ -63,29 +77,4 @@ class Product < ApplicationRecord
   def clean_text(text)
     text.gsub!("\n", "").strip
   end
-
 end
-
-# title from review
-# page.css('.crIFrameReviewList').search('table').children.search('div b').first.children.first.text
-
-# date
-# page.css('.crIFrameReviewList').search('table').children.search('div nobr').first.children.first.text
-
-# rating from review
-# page.css('.crIFrameReviewList').search('table').children.search('div span img').first.attributes['title'].value
-
-# Reviewer
-# page.css('.crIFrameReviewList').search('table').children.search('div a').children.first.text ou div div div a
-
-# Review
-# remove all tiny
-# page.css('.crIFrameReviewList').search('table').children.search('div//.tiny').remove
-# remove all div
-# page.css('.crIFrameReviewList').search('table').children.search('div//div').remove
-# get review
-# page.css('.crIFrameReviewList').search('table').children.search('div').text
-
-# page.css('.crIFrameReviewList').search('table > tr > td > div').length
-# page.css('.crIFrameReviewList').search('table > tr > td > div > div').remove
-
